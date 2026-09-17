aa17ed0d9416d32d5129b7b28c4338f7f17a768f
Solon Guedes <solon.guedesc@gmail.com>
2026-09-17 08:44:07 -0300

    add extract_job function to parse title, company, location and link

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 
 
 def fetch_page(url:str) -> tuple:
@@ -15,6 +15,32 @@ def parse_listings (html: str) -> list:
     listings = soup.find_all("li", class_="new-listing-container")
     return listings
 
+def extract_job(listing: Tag) -> dict:
+    job_title = listing.find("span", class_="new-listing__header__title__text")
+    if not job_title:
+        return None
+    title_text = job_title.text
+    
+    company_name = listing.find("p", class_="new-listing__company-name")
+    if not company_name:
+        return None
+    company_text = company_name.text
+    
+    location_name = listing.find("p", class_="new-listing__company-headquarters")
+    location_status = location_name.text if location_name else "Not specified"
+    
+    link_element = listing.find("a", class_="listing-link--unlocked")
+    job_url = link_element["href"]
+    full_url = f"https://weworkremotely.com{job_url}"
+    
+    extracted_job = {
+        "title": title_text.strip(),
+        "company": company_text.strip(),
+        "location": location_status.strip(),
+        "link": full_url.strip()
+    }
+    return extracted_job
+        
 
 if __name__ == "__main__":
     html, status = fetch_page("https://weworkremotely.com/categories/remote-design-jobs")
@@ -25,12 +51,10 @@ if __name__ == "__main__":
     print("Listings found:", len(listings))
     
     for listing in listings:
-        title_element = listing.find("span", class_="new-listing__header__title__text")
-        if not title_element:
-            print("- (no title found)")
+        job = extract_job(listing)
+        if not job:
             continue
-    
-        print("-", title_element.text)
+        print(job)
             
             
     
